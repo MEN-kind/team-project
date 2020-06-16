4f1a21197c189f7ad0d11e18f8b4c83e0ead5b4f
Jeerrryyyy <j.braun@findologic.com>
2020-06-16 14:46:10 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/team_project.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/team_project.dir/main.cpp.obj"
+  "team_project.pdb"
+  "team_project.exe"
+  "team_project.exe.manifest"
+  "libteam_project.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/team_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+int main() {
+    double number1, number2;
+
+    cout << "Please write me the first number:" << endl;
+    cin >> number1;
+
+    cout << "Please write me the second number:" << endl;
+    cin >> number2;
+
+    // Do the next stuff
+
+    return 0;
+}
